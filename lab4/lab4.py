f7ac88f8306f165c35dbe78bb18a2e455b166aad
darius <lepadatu.darius@yahoo.com>
2025-11-26 13:02:18 +0200

    added cosine similarity for products

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -1,18 +1,17 @@
 import json
+
+import numpy as np
 from bs4 import BeautifulSoup
 import spacy
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 nlp = spacy.load("en_core_web_sm")
 
 class Product:
-
-    def __init__(self, name, id):
+    def __init__(self, name, id_):
         self.name = name
-        self.id = id
-        self.vectorized = None
-
-    def compute_cosine_similarity(self):
-        self.vectorized = [1]
+        self.id = id_
 
 
 def read_json(filename):
@@ -26,27 +25,47 @@ def read_json(filename):
     return d
 
 
-def preprocess_text(text: str) -> list[str]:
-    """
-    Pipeline:
-    1. tokenize
-    2. lowercase
-    3. lemmatize
-    4. remove stopwords + punctuation + non-alphabetic tokens
-    """
+def preprocess_text(text):
     doc = nlp(text)
-    tokens = [token.lemma_.lower() for token in doc if not token.is_stop and token.is_alpha ]
-    return tokens
-
+    tokens = [token.lemma_.lower()
+            for token in doc
+            if not token.is_stop and token.is_alpha]
+    return " ".join(tokens)
 
 
 if __name__ == '__main__':
+
     products_data = read_json('tesco_sample.json')
-    for product in products_data:
-        html = product.get('description', '')
-        text = BeautifulSoup(html, 'html.parser').get_text(" ", strip=True)
-        processed_text = preprocess_text(text)
-        print(processed_text)
-        print("==========================")
+    print("Loaded json")
+
+    products = []
+    corpus = []
+
+    print("Preprocessing text")
+    for pd in products_data:
+        html = pd.get('description', '')
+        raw_text = BeautifulSoup(html, 'html.parser').get_text(" ", strip=True)
+
+        preprocessed_text = preprocess_text(raw_text)
+
+        p = Product(name=pd.get('name', ''), id_=pd.get('gtin13', ''))
+        products.append(p)
+        corpus.append(preprocessed_text)
+
+    print("Vectorizing text")
+
+    # Vectorize all descriptions of products
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform(corpus)
 
 
+    # Compute similarity matrix
+    sim_matrix = cosine_similarity(tfidf_matrix)
+
+    # Fill the identity diagonal with 1 values in order to remove self comparison
+    np.fill_diagonal(sim_matrix, 1)
+
+    # Find out the lowest cosine similarity
+    i, j = np.unravel_index(sim_matrix.argmin(), sim_matrix.shape)
+
+    print(f"Most similar products are {products[i].name} and {products[j].name}")
